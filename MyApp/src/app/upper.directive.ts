76d3dd9125da954b4c7adbcd27670fc0e2fa2ba6
mshahhat <mohammed.shahhat@gmail.com>
2017-04-28 05:12:21 -0500

    Update.

---
+++ MyApp/src/app/upper.directive.ts
@@ -0,0 +1,17 @@
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+} from '@angular/core';
+
+@Directive({
+  selector: '[upper]'
+})
+export class UpperDirective {
+
+  constructor(private element: ElementRef,
+              private renderer: Renderer2) {
+    renderer.setStyle(element.nativeElement, 'text-transform', 'uppercase');
+  }
+
+}
